78c57a546c6323e2cac08f973e9dec904dfb0d75
hua <wiwi1513@gmail.com>
2023-05-03 01:38:25 +0800

    Added Art slide.

--- src/routes/about_chanel/+page.svelte
+++ src/routes/about_chanel/+page.svelte
@@ -2,6 +2,7 @@
 	import Logo from './Logo.svelte';
 	import Signature from './Signature.svelte';
 	import Slide from './Slide.svelte';
+	import Art from './Art.svelte';
 </script>
 
 <main class=" h-screen w-screen">
@@ -36,23 +37,18 @@
 	</div>
 	<div class="mx-auto flex w-[590px] flex-col items-center">
 		<span class="text-[30px] font-[600]">「願我的傳奇淵遠流長，恆久愉悅！」*</span>
-		<span class="text-center text-[14px] font-[300] italic"
-			>* 《香奈兒的孤傲與顛世》（The Allure of Chanel），作者：保羅•莫朗（Paul Morand），©
-			1976年，Hermann www.editions-hermann.fr</span
-		>
+		<span class="text-center text-[14px] font-[300] italic">
+			{`* 《香奈兒的孤傲與顛世》（The Allure of Chanel），作者：保羅•莫朗（Paul Morand），© 1976年，Hermann www.editions-hermann.fr`}
+		</span>
 		<span class="text-[14px] font-[300]">© Man Ray Trust / ADAGP，巴黎，2016年</span>
 	</div>
 	<div class="mx-auto w-[590px] text-center text-[14px] font-[300]">
-		<span class=""
-			>嘉柏麗‧香奈兒以自己選擇的方式生活，從歷經艱辛的年幼孤女到晉身為事業成功的女商人，造就出她與眾不同、大膽率真的個性，讓她遠遠領先所處的時代。她擁有忠誠真摯的友誼，享受著親密熾熱的愛戀，她對文化、探索和旅行有著無法抑制的渴望，這一切，都對她的性格帶來重要影響。她讓女性的穿著擺脫束縛，化繁為簡，並將男裝特色融入女裝設計，呈現出具有遠見的魅力風格，體現洋溢現代氣息的永恆經典。她以從容大方的姿態配戴珍珠和鑽石，搭配經典香水彰顯個人風格。她是個前衛女性，是一位以自己的生活方式與多重面貌為她一手創辦的品牌樹立價值理念的先鋒人物。同時，持續不斷為女性帶來啟發與靈感。
+		<span class="">
+			{`嘉柏麗‧香奈兒以自己選擇的方式生活，從歷經艱辛的年幼孤女到晉身為事業成功的女商人，造就出她與眾不同、大膽率真的個性，讓她遠遠領先所處的時代。她擁有忠誠真摯的友誼，享受著親密熾熱的愛戀，她對文化、探索和旅行有著無法抑制的渴望，這一切，都對她的性格帶來重要影響。她讓女性的穿著擺脫束縛，化繁為簡，並將男裝特色融入女裝設計，呈現出具有遠見的魅力風格，體現洋溢現代氣息的永恆經典。她以從容大方的姿態配戴珍珠和鑽石，搭配經典香水彰顯個人風格。她是個前衛女性，是一位以自己的生活方式與多重面貌為她一手創辦的品牌樹立價值理念的先鋒人物。同時，持續不斷為女性帶來啟發與靈感。`}
 		</span>
 	</div>
 	<Slide />
-	<div class="h-[1323px] w-full bg-white text-black">
-		<h2 class="py-[72px] text-center text-[40px] font-[600] leading-[45px]">
-			藝術家眼中的 COCO 香奈兒
-		</h2>
-	</div>
+	<Art />
 	<div class="h-[1353px] w-full bg-zinc-800 text-white">
 		<h2 class="py-[72px] text-center text-[40px] font-[600] leading-[45px]">當 COCO 邂逅香奈兒</h2>
 	</div>
