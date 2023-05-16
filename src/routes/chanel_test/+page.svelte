413efcbb218eaededa4d6fe45b2f776b59e82261
hua5155 <s17115155@stu.edu.tw>
2023-05-16 10:31:11 +0800

    Video player prototype.

--- src/routes/chanel_test/+page.svelte
+++ src/routes/chanel_test/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import AnimationPrototype from './AnimationPrototype.svelte';
 	import SlideRefactoring from './SlideRefactoring.svelte';
+	import VideoPrototype from './VideoPrototype.svelte';
+	import VideoPlayer from '$lib/component/Chanel/When_coco_meets_chanel/VideoPlayer.svelte';
 </script>
 
-<main class="h-fit w-screen bg-black">
+<main class="h-fit w-screen bg-slate-900">
 	<!-- <AnimationPrototype /> -->
-	<SlideRefactoring />
+	<!-- <SlideRefactoring /> -->
+	<!-- <VideoPrototype /> -->
+	<VideoPlayer />
 </main>
 
 <style>
