5aec9a2abfbc2d0e1c987da74d52ffd9121b50ff
hua5155 <s17115155@stu.edu.tw>
2023-05-19 12:24:04 +0800

    Draggable slider for cards Join button

--- src/routes/social_coffee_house/+page.svelte
+++ src/routes/social_coffee_house/+page.svelte
@@ -4,42 +4,104 @@
 	let modalFlag = false;
 	let animationIn = false;
 	let animationOut = false;
+
+	const dummyData = [
+		{
+			id: 50,
+			date: '2023.05.19',
+			heading: 'Heading',
+			name: 'tom'
+		},
+		{
+			id: 51,
+			date: '2023.05.20',
+			heading: 'Heading',
+			name: 'karl'
+		},
+		{
+			id: 52,
+			date: '2023.05.21',
+			heading: 'Heading',
+			name: 'ion'
+		},
+		{
+			id: 53,
+			date: '2023.05.22',
+			heading: 'Heading',
+			name: 'brandon'
+		}
+	];
+	let selected = dummyData[0];
+	let clickBlock = false; // try to find better solution
+
+	let scroll = 0;
+	let referenceX = 0;
+	let dragFlag = false;
+	let slideElement: HTMLElement;
+	const handleDrag = (e: MouseEvent) => {
+		if (dragFlag === false) {
+			return;
+		}
+
+		clickBlock = true;
+
+		// let referenceX = slideElement.getBoundingClientRect().left;
+		let deviation = e.pageX - referenceX;
+		scroll += deviation;
+
+		referenceX = e.pageX;
+	};
 </script>
 
 <main class="absolute left-0 top-0 h-screen w-screen max-w-full">
 	<div class="mx-auto mt-10 h-fit w-fit">
-		<p>
-			{`modalFlag : ${modalFlag}`}
-		</p>
-		<p>
-			{`animationIn : ${animationIn}`}
-		</p>
-		<p>
-			{`animationOut : ${animationOut}`}
-		</p>
+		<p>{`modalFlag : ${modalFlag}`}</p>
+		<p>{`animationIn : ${animationIn}`}</p>
+		<p>{`animationOut : ${animationOut}`}</p>
+		<p>{`dragFlag : ${dragFlag} (scroll : ${scroll}), clickBlock : ${clickBlock}`}</p>
 	</div>
 
-	<div class="mx-auto mt-10 h-fit w-fit">
-		<button
-			class=""
-			on:click={() => {
-				modalFlag = true;
-				animationIn = true;
-			}}
+	<div class="mt-10 border-b-2 border-t-2 border-[#003e59] bg-[#00a1c0]">
+		<ul
+			class="draggable mx-auto mt-10 flex h-[500px] w-[500px] flex-row space-x-24"
+			style="--position: {scroll}px;"
+			bind:this={slideElement}
 		>
-			<Card id={50} date="2023.05.19" heading="Heading">
-				<div class="flex flex-row space-x-5">
-					<div class="flex flex-row space-x-1">
-						<div class="h-[110px] w-[110px] border-2 border-[#003e59] bg-pink-400" />
-						<div
-							class="h-fit w-fit bg-[#ffde05] text-[20px] font-[400] leading-[1.6rem] text-[#003e59]"
-						>
-							Guest name
-						</div>
-					</div>
-				</div>
-			</Card>
-		</button>
+			{#each dummyData as card, index}
+				<li
+					on:mousedown={(e) => {
+						// console.log('mouse down');
+						referenceX = e.pageX;
+						dragFlag = true;
+					}}
+				>
+					<button
+						class=""
+						on:click={() => {
+							if (clickBlock === true) {
+								return;
+							}
+							selected = dummyData[index];
+							modalFlag = true;
+							animationIn = true;
+						}}
+					>
+						<Card id={card.id} date={card.date} heading={card.heading}>
+							<div class="flex flex-row space-x-5">
+								<div class="flex flex-row space-x-1">
+									<div class="h-[110px] w-[110px] border-2 border-[#003e59] bg-pink-400" />
+									<div
+										class="h-fit w-fit bg-[#ffde05] text-[20px] font-[400] capitalize leading-[1.6rem] text-[#003e59]"
+									>
+										{card.name}
+									</div>
+								</div>
+							</div>
+						</Card>
+					</button>
+				</li>
+			{/each}
+		</ul>
 	</div>
 
 	{#if modalFlag === true}
@@ -69,14 +131,14 @@
 						X
 					</div>
 				</button>
-				<Card id={50} date="2023.05.19" heading="Heading">
+				<Card id={selected.id} date={selected.date} heading={selected.heading}>
 					<div class="flex flex-row space-x-5">
 						<div class="flex flex-row space-x-1">
 							<div class="h-[110px] w-[110px] border-2 border-[#003e59] bg-pink-400" />
 							<div
-								class="h-fit w-fit bg-[#ffde05] text-[20px] font-[400] leading-[1.6rem] text-[#003e59]"
+								class="h-fit w-fit bg-[#ffde05] text-[20px] font-[400] capitalize leading-[1.6rem] text-[#003e59]"
 							>
-								Guest name
+								{selected.name}
 							</div>
 						</div>
 					</div>
@@ -84,17 +146,51 @@
 			</div>
 		</div>
 	{/if}
+
+	<div
+		class="box-shadow fixed bottom-10 left-10 h-fit w-fit transition-transform duration-500 hover:scale-110"
+	>
+		<div class="z-0 h-fit w-fit">
+			<img class="h-[160px] w-[160px]" src="social_coffee_house/circle_center.png" alt="" />
+		</div>
+		<div class="rotate absolute left-0 top-0 z-10 flex h-full w-full items-center justify-center">
+			<img class="h-[135px] w-[135px]" src="social_coffee_house/circle_txt.png" alt="" />
+		</div>
+	</div>
 </main>
 
 <svelte:window
 	on:keypress={(e) => {
-		console.log(`window : ${e.key}`);
+		// console.log(`window : ${e.key}`);
+	}}
+	on:mousemove={(e) => {
+		// console.log('mouse move');
+		handleDrag(e);
+	}}
+	on:mouseup={() => {
+		// console.log('mouse up');
+		setTimeout(() => {
+			clickBlock = false;
+		}, 10);
+		dragFlag = false;
 	}}
 />
 
 <style>
 	.box-shadow {
 		filter: drop-shadow(-3px 3px 0px #003e59);
+	}
+
+	.rotate {
+		animation: rotate 11000ms linear infinite;
+	}
+	@keyframes rotate {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
 	}
 
 	.modal-fadeIn {
@@ -119,5 +215,12 @@
 		100% {
 			opacity: 0;
 		}
+	}
+
+	.slide {
+		transform: translateX();
+	}
+	.draggable {
+		transform: translateX(var(--position));
 	}
 </style>
