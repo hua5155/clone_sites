8513ec8401beea5765f0209664e5a52b7f9d3f3f
hua5155 <s17115155@stu.edu.tw>
2024-10-21 11:10:01 +0800

    home page RWD fix.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,31 +19,28 @@
 	];
 </script>
 
-<main class="h-screen w-screen bg-black p-20 text-3xl font-semibold text-gray-200">
-	<h1 class="w-[60ch]">
-		<span class="text-gray-500">Disclaimers</span>
-		<ol class="list-outside list-decimal space-y-5 pl-8 pt-5">
-			<li>
-				I didn't work for any of these websites, I'm just trying to practice Svelte &Tailwind by
-				copying these websites.
-			</li>
-			<li>
-				I do not own the copyright to the assests I used, If I violated your IP please notify me at
-				s17115155@stu.edu.tw, I will remove the page.
-			</li>
-		</ol>
-	</h1>
+<main class="h-fit min-h-lvh w-dvw bg-black p-10 font-semibold text-gray-200 xl:p-20">
+	<h1 class="text-3xl text-gray-500">Disclaimers</h1>
+	<ol class="list-outside list-decimal space-y-5 pl-8 pt-5 text-sm xl:text-2xl xl:[&>li]:w-[70ch]">
+		<li>
+			I didn't work for any of these websites, I'm just trying to practice Svelte &Tailwind by
+			copying these websites.
+		</li>
+		<li>
+			I do not own the copyright to the assests I used, If I violated your IP please notify me at
+			s17115155@stu.edu.tw, I will remove the page.
+		</li>
+	</ol>
 
-	<div class="pt-20">
-		<span class="text-gray-500">Websites</span>
-		<ul class="space-y-2 pt-5">
-			{#each urls as url}
-				<li>
-					<a class="" href={url.href}>{url.text}</a>
-				</li>
-			{/each}
-		</ul>
-	</div>
+	<div class="w-full pt-20"></div>
+	<h1 class="text-3xl text-gray-500">Websites</h1>
+	<ul class="space-y-2 pt-5 text-xl xl:text-2xl">
+		{#each urls as url}
+			<li>
+				<a class="" href={url.href}>{url.text}</a>
+			</li>
+		{/each}
+	</ul>
 </main>
 
 <style></style>
